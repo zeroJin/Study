2d62ed748b621c38b3305f3bc647efef2e8fe76e
jinzhenguo <jinzhenguo@evermarker.net>
2017-01-09 19:28:00 +0800

    adGame

---
+++ antbuild/src/com/study/oop/adGame/DotCom.java
@@ -0,0 +1,36 @@
+package com.study.oop.adGame;
+
+import java.util.ArrayList;
+
+public class DotCom {
+
+	private ArrayList<String> locationCells;
+	private String name;
+	
+	public void setLocationCells(ArrayList<String> loc){
+		this.locationCells = loc;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String checkYourself(String userInput){
+		String result = "miss";
+		
+		int index = locationCells.indexOf(userInput);
+		
+		if(index >= 0){
+			
+			locationCells.remove(index);
+			
+			if(locationCells.isEmpty()){
+				System.out.println("Ouch! You sunk " + name + "  :  ");
+				return "kill";
+			}else{
+				return "hit";
+			}
+		}
+		return result;
+	}
+}
--- antbuild/src/com/study/oop/game/SimpleDotCom.java
+++ antbuild/src/com/study/oop/game/SimpleDotCom.java
@@ -1,28 +1,39 @@
 package com.study.oop.game;
 
+import java.util.List;
+
 public class SimpleDotCom {
 
-	int[] locationCells;
+//	int[] locationCells;
+	private List<String> list;
 	int numOfHits;
+	
+	void setLocationCells(List<String> loc){
+		this.list = loc;
+	}
 	
 	String checkYourself(String stringGuess){
 		int guess = Integer.parseInt(stringGuess);
 		String result = "miss";
-		for(int cell : locationCells){
-			if(guess == cell){
-				result = "hit!";
-				numOfHits ++ ;
-				break;
+//		使用数组实现的游戏逻辑
+//		for(int cell : locationCells){
+//			if(guess == cell){
+//				result = "hit!";
+//				numOfHits ++ ;
+//				break;
+//			}
+//		}
+		int index = list.indexOf(stringGuess);
+		if(index >= 0){
+			list.remove(index);
+			if(list.isEmpty()){
+				result = "kill";
 			}
 		}
-		if(numOfHits == locationCells.length){
-			return "kill!";
-		}
+//		if(numOfHits == locationCells.length){
+//			return "kill!";
+//		}
 		System.out.println("result: " + result);
 		return result;
-	}
-	
-	void setLocationCells(int[] loc){
-		this.locationCells = loc;
 	}
 }
